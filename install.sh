3bc699113999b1ed312c166b2c0bf2217b48aad9
zycbobby <zuo.yc@qq.com>
2015-02-04 22:38:58 +0800

    init

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo cp /etc/apt/sources.list /etc/apt/sources.list
+sudo cp ./resources/sources.list /etc/apt/sources.list 
+
+sudo apt-get update
+
+sudo apt-get install terminator zsh
+
+wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+
+chsh -s /bin/zsh
