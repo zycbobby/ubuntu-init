dee03125c131d46f5b281dc8525261869f90ad3e
zycbobby <zuo.yc@qq.com>
2015-02-08 21:40:22 +0800

    Add install nodejs

---
+++ ubuntu-sh-home/install_nodejs.sh
@@ -0,0 +1,14 @@
+cd  ~/Downloads
+
+wget http://nodejs.org/dist/v0.12.0/node-v0.12.0.tar.gz
+
+sudo apt-get install build-essential gcc g++ 
+
+tar -xvf node-v0.12.0.tar.gz
+
+cd node-v0.12.0
+
+make
+
+sudo make install
+
