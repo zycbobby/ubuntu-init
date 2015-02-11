4d0e3b8c435d316d0a6621ee34aa678d274d3516
zycbobby <zuoyuchu@gmail.com>
2015-02-11 11:24:21 +0800

    fix rbenv

--- ubuntu-sh-office/install_ruby.sh
+++ ubuntu-sh-office/install_ruby.sh
@@ -6,6 +6,11 @@ echo 'eval "$(rbenv init -)"' >> ~/.zshrc
 
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
+rbenv install 2.1.5
+rbenv global 2.1.5
+
+gem install bundle
+
 
 
 
