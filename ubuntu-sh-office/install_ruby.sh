c06e28e33d2a352ae51fb5fd10164e01b658aa88
zycbobby <zuoyuchu@gmail.com>
2015-02-11 10:47:29 +0800

    add rbenv

---
+++ ubuntu-sh-office/install_ruby.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+
+git clone https://github.com/sstephenson/rbenv.git ~/.zshrc
+
+echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+
+
+
