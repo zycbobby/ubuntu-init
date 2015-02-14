fd616d7215f1338259b2385e7fd9d741d04e8b64
zycbobby <zuo.yc@qq.com>
2015-02-14 14:39:59 +0800

    Add tomcat install and user config

--- install.sh
+++ install.sh
@@ -13,6 +13,9 @@ chsh -s /bin/zsh
 # install gnome
 sudo apt-get install gnome-shell gnome-tweak-tool
 
+# sysv-rc-conf
+sudo apt-get install sysv-rc-conf
+
 #install docker
 sudo apt-get install docker.io
 
@@ -23,6 +26,12 @@ sudo apt-get install openjdk-7-jdk
 sudo apt-get install maven
 
 # install tomcat
+sudo apt-get install tomcat7 tomcat7-admin tomcat7-docs tomcat7-common
+TOMCAT__CONF_HOME='/var/lib/tomcat7/conf/'
+TOMCAT_USER_FILENAME='tomcat-users.xml'
+sudo cp $TOMCAT_CONF_HOME+$TOMCAT_USER_FILENAME $TOMCAT_CONF_HOME+$TOMCAT_USER_FILENAME+'.bak'
+sudo cp ./resources/$TOMCAT_USER_FILENAME $TOMCAT_CONF_HOME+$TOMCAT_USER_FILENAME
 
+# install maven
 # Apache benchmark
 sudo apt-get install apache2-utils
