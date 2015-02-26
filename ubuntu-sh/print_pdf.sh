2776e64b5e0350ccdf0f3c7691e74e268136e642
zycbobby <zuoyuchu@gmail.com>
2015-02-26 15:41:30 +0800

    Add comments

--- ubuntu-sh/print_pdf.sh
+++ ubuntu-sh/print_pdf.sh
@@ -1,3 +1,5 @@
 sudo apt-get install enscript
 
+# Leave the output to file file.  If the file is `-', enscript sends the output to the standard output
+# stdout.
 enscript my_text_file.txt -o - | ps2pdf - output.pdf
