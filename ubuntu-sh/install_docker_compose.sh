0becadbed5083108bea51384b23d39ef986e4197
zycbobby <zuoyuchu@gmail.com>
2015-02-27 18:50:39 +0800

    Add docker compose

---
+++ ubuntu-sh/install_docker_compose.sh
@@ -0,0 +1,2 @@
+sudo curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo hmod +x /usr/local/bin/docker-compose
