358792746465fbc85e485825ad36d91307ac0014
zycbobby <zuoyuchu@gmail.com>
2015-03-19 13:04:56 +0800

    Add mount smb

--- ubuntu-sh/mount_smb.sh
+++ ubuntu-sh/mount_smb.sh
@@ -1,3 +1,8 @@
 sudo apt-get install cifs-utils
 
-sudo echo "//servername/sharename  /media/windowsshare  cifs  username=msusername,password=mspassword,iocharset=utf8,sec=ntlm  0  0" >> /etc/fstab
+
+#sudo echo "//servername/sharename  /media/windowsshare  cifs  username=msusername,password=mspassword,iocharset=utf8,sec=ntlm  0  0" >> /etc/fstab
+
+# sudo mount -a
+
+# https://wiki.ubuntu.com/MountWindowsSharesPermanently
